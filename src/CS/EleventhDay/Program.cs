a1fc49581212fdf4562f3f57c286d8c5c556a592
Sergei Karimov <billy.medison@outlook.com>
2016-12-28 06:17:26 +0300

    Added Eleventh day into CS

--- src/CS/EleventhDay/Program.cs
+++ src/CS/EleventhDay/Program.cs
@@ -5,67 +5,40 @@ using System.Linq;
 
 public class BigMatrix
 {
-    int[][] _bigMatrix;
-
-    public BigMatrix(int[][] bigMatrix)
+    public static List<int[,]> SeparateMatrix(int[][] bigMatrix)
     {
-        _bigMatrix = bigMatrix;
-    }
-
-    public List<int[,]> SeparateMatrix()
-    {
-        int[,] small_matrix = new int[3,3];
-        int x = _bigMatrix.GetLength(0);
-        int y = _bigMatrix[0].GetLength(0);
+        int x = bigMatrix.GetLength(0);
+        int y = bigMatrix[0].GetLength(0);
         List<int[,]> sm = new List<int[,]>();
-        for(int ix = 0; ix < x - 3; ix++)
+        for(int ix = 0; ix <= x - 3; ix++)
         {
-            for (int iy = 0; iy < y - 3; iy++)
+            for (int iy = 0; iy <= y - 3; iy++)
             {
-                small_matrix[0,0] = _bigMatrix[ix + 0][iy + 0];
-                small_matrix[0,1] = _bigMatrix[ix + 0][iy + 1];
-                small_matrix[0,2] = _bigMatrix[ix + 0][iy + 2];
-                small_matrix[1,0] = _bigMatrix[ix + 1][iy + 0];
-                small_matrix[1,1] = _bigMatrix[ix + 1][iy + 1];
-                small_matrix[1,2] = _bigMatrix[ix + 1][iy + 2];
-                small_matrix[2,0] = _bigMatrix[ix + 2][iy + 0];
-                small_matrix[2,1] = _bigMatrix[ix + 2][iy + 1];
-                small_matrix[2,2] = _bigMatrix[ix + 2][iy + 2];
+                int[,] small_matrix = new int[3, 3];
+                small_matrix[0,0] = bigMatrix[ix + 0][iy + 0];
+                small_matrix[0,1] = bigMatrix[ix + 0][iy + 1];
+                small_matrix[0,2] = bigMatrix[ix + 0][iy + 2];
+                small_matrix[1,0] = bigMatrix[ix + 1][iy + 0];
+                small_matrix[1,1] = bigMatrix[ix + 1][iy + 1];
+                small_matrix[1,2] = bigMatrix[ix + 1][iy + 2];
+                small_matrix[2,0] = bigMatrix[ix + 2][iy + 0];
+                small_matrix[2,1] = bigMatrix[ix + 2][iy + 1];
+                small_matrix[2,2] = bigMatrix[ix + 2][iy + 2];
                 sm.Add(small_matrix);
-                small_matrix.Initialize();
             }
         }
-
         return sm;
     }
 }
 
 public class Hourglass
 {
-    private int[,] _matrix = new int[3, 3];
-
-    private Hourglass()
+    public static int Calculate(int[,] matrix)
     {
-    }
-
-    private void SetMatrix(int[,] matrix)
-    {
-        _matrix = matrix;
-    }
-
-    private int Calculate()
-    {
-        return _matrix[0, 0] + _matrix[0, 1] + _matrix[0, 2] + _matrix[1, 1] + _matrix[2, 0] + _matrix[2, 1] + _matrix[2, 2];
-    }
-
-    public Hourglass(int[,] matrix)
-    {
-        this.SetMatrix(matrix);
-    }
-
-    public int Summa
-    {
-        get { return this.Calculate(); }
+        if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+            return matrix[0, 0] + matrix[0, 1] + matrix[0, 2] + matrix[1, 1] + matrix[2, 0] + matrix[2, 1] + matrix[2, 2];
+        else
+            return -999;
     }
 }
 
@@ -74,13 +47,18 @@ class EleventhDay
     static void Main(string[] args)
     {
         string[] str = new string[6];
-        str[0] = "1 1 1 0 0 0\n";
-        str[1] = "0 1 0 0 0 0\n";
-        str[2] = "1 1 1 0 0 0\n";
-        str[3] = "0 0 2 4 4 0\n";
-        str[4] = "0 0 0 2 0 0\n";
-        str[5] = "0 0 1 2 4 0\n";
-
+        //str[0] = "1 1 1 0 0 0\n";
+        //str[1] = "0 1 0 0 0 0\n";
+        //str[2] = "1 1 1 0 0 0\n";
+        //str[3] = "0 0 2 4 4 0\n";
+        //str[4] = "0 0 0 2 0 0\n";
+        //str[5] = "0 0 1 2 4 0\n";
+        str[0] = "0 -4 -6 0 -7 -6\n";
+        str[1] = "-1 -2 -6 -8 -3 -1\n";
+        str[2] = "-8 -4 -2 -8 -8 -6\n";
+        str[3] = "-3 -1 -2 -5 -7 -4\n";
+        str[4] = "-3 -5 -3 -6 -6 -6\n";
+        str[5] = "-3 -6 0 -8 -6 -7\n";
         int[][] arr = new int[6][];
         for (int arr_i = 0; arr_i < 6; arr_i++)
         {
@@ -89,21 +67,22 @@ class EleventhDay
             arr[arr_i] = arr_temp.Select(x => Convert.ToInt32(x)).ToArray();
         }
 
-        BigMatrix bm = new BigMatrix(arr);
-        List<int[,]> arrs = bm.SeparateMatrix();
-
-        List<Hourglass> hgs = new List<Hourglass>();
-
-        int max = 0;
-        foreach(var item in hgs)
+        List<int[,]> arrs = BigMatrix.SeparateMatrix(arr);
+        int max = -999;
+        foreach (var item in arrs)
         {
-            if (item.Summa > max)
-                max = item.Summa;
-
-            Console.WriteLine(item.Summa);
+            int res = Hourglass.Calculate(item);
+            if (res > max)
+                max = res;
         }
-
+        
         Console.WriteLine(max);
         Console.ReadLine();
     }
+
+    [TestMethod]
+    static void Test1()
+    {
+
+    } 
 }
