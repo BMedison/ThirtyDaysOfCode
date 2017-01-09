d510e6f8809dd0479d73b7283406ead16f80c677
Billy Medison <billy.medison@outlook.com>
2017-01-09 17:55:44 +0300

    Added NUnit test module

---
+++ src/CS/EighteenthDay/EighteenthDayTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace EighteenthDay.Tests
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void pushCharacterTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void enqueueCharacterTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void popCharacterTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void dequeueCharacterTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/CS/FourteenthDay/SolutionTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using EighteenthDay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EighteenthDay.Tests
+{
+    [TestFixture()]
+    public class SolutionTests
+    {
+        [Test()]
+        public void pushCharacterTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void enqueueCharacterTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void popCharacterTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void dequeueCharacterTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
