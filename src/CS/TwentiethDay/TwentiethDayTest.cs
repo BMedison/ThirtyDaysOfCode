82cddfbffa31f6086846a6e3cd32fd268aa007ba
Sergei Karimov <billy.medison@outlook.com>
2017-01-11 07:54:48 +0300

    Twentieth day was added

---
+++ src/CS/TwentiethDay/TwentiethDayTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+using TwentiethDay.BubbleSort;
+
+namespace TwentiethDay
+{
+    [TestFixture]
+    class TwentiethDayTest
+    {
+        private IBubbleSort bs;
+
+        [SetUp]
+        public void Setup()
+        {
+            bs = new BubbleSortOne();
+        }
+
+        [Test]
+        public void SortingTest()
+        {
+            int[] a = new int[3] { 2, 3, 1 };
+            int[] b = bs.Sort(a);
+            Assert.AreEqual(new int[]{ 1, 2, 3 }, b);
+        }
+    }
+}
