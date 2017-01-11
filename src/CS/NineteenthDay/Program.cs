2b01f91594c7c42533c31ecb77a4b4434ced00a3
Billy Medison <billy.medison@outlook.com>
2017-01-11 09:26:48 +0300

    *

---
+++ src/CS/NineteenthDay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineteenthDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            int i = 1;
+            int summ = 1;
+
+            while(i++ < n)
+            {
+                if (n % i == 0)
+                    summ += i;
+            }
+
+            Console.WriteLine(summ);
+            Console.ReadLine();
+        }
+    }
+}
