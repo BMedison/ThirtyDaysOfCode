901ff23b448f3930eb8ca28c53f58186f015e702
Billy Medison <billy.medison@outlook.com>
2017-01-12 17:29:39 +0300

    Twenty second day was added

---
+++ src/CS/TwentySecondDay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void TestName()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+            Assert.Equal(2, 2 * 3);
+        }
+    }
+}
